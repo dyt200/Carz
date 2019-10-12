2d230b6a9fe76758dadb20d4c58de8cf61f8c42e
Ben PicklesOnion <39406726+RabidMexican@users.noreply.github.com>
2019-10-12 08:49:43 +0200

    Testing git integration

--- app/src/main/java/com/example/carz/MainActivity.java
+++ app/src/main/java/com/example/carz/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //this is a test!
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
     }
