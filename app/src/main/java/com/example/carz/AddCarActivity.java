e56ae6e45c75cb51cd76c17c7c1c478320d3f0bf
Ben PicklesOnion <39406726+RabidMexican@users.noreply.github.com>
2019-10-17 19:06:34 +0200

    [create_user.xml] updated layout

--- app/src/main/java/com/example/carz/AddCarActivity.java
+++ app/src/main/java/com/example/carz/AddCarActivity.java
@@ -1,11 +1,32 @@
 package com.example.carz;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddCarActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_car);
+
+        Spinner typeSpinner = findViewById(R.id.type_spinner);
+        ArrayAdapter<CharSequence> typeAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.type_array,
+                android.R.layout.simple_spinner_item
+        );
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        typeSpinner.setAdapter(typeAdapter);
+
+        Spinner makeSpinner = findViewById(R.id.make_spinner);
+        ArrayAdapter<CharSequence> makeAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.make_array,
+                android.R.layout.simple_spinner_item
+        );
+        makeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        makeSpinner.setAdapter(makeAdapter);
     }
 }
